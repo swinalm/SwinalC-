359e4d1b7a3759d4192214296be0dd3240e35f34
addld <addld@PRANAY>
2024-01-09 08:54:07 +0530

    Add project files.

---
+++ Data_Type/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Byte");
+        Console.WriteLine("Size of Byte" + sizeof(byte));
+        Console.WriteLine("Minimum Value of Byte" + byte.MinValue);
+        Console.WriteLine("Maximum Value of Byte" + byte.MaxValue);
+        Console.WriteLine("************************************************************");
+
+        Console.WriteLine(sizeof(short));
+        Console.WriteLine(sizeof(int));
+        Console.WriteLine(sizeof(long));
+
+        Console.ReadLine();
+    }
+}
+
---
+++ First_Project/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        int vari_name; // declration
+        vari_name = 100; // value assign 
+
+        int rollnumber = 101;
+        Console.WriteLine(vari_name); //100
+        Console.WriteLine(rollnumber); //101
+    }
+}
