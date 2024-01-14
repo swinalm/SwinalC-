9429d5af2d16c47f1a75ffc27c0d343b9c2366a4
gauravshahare1 <121213204+gauravshahare1@users.noreply.github.com>
2024-01-14 11:23:20 +0530

    type conversion

---
+++ Type_Conversion/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+byte b1 = 255;
+int i1 = b1; // byte => int
+Console.WriteLine(i1); // 255
+
+// short s = i1; // int => short
+// Cannot implicitly convert type 'int' to 'short'. An explicit conversion exists (are you missing a cast?)	4_TypeConversion C:\Users\user\source\repos\B21CSharpDemos\4_TypeConversion\Program.cs	5	N/A
+
+short s = (short)i1;
+Console.WriteLine(s);
+
+float f = i1; // int => float
+Console.WriteLine(f);
+
+f = 25.50f;
+int i2 = (int)f; // float => int
+Console.WriteLine(i2);
+
+string s1 = "1";
+string s2 = "1";
+
+
+
+
+
+i2 = int.Parse(s1);
+Console.WriteLine(i2);
+
+
+int n1 = int.Parse(s1);
+int n2 = int.Parse(s2);
+
+Console.WriteLine(s1 + s2); // 11
+Console.WriteLine(n1 + n2); // 2
+
+s1 = "1OO";
+// int n3 = int.Parse(s1); // throws runtime error
+//Console.WriteLine(n3);
+int n3;
+ int.TryParse(s1, out n3); // in case of conversion error => handles runtime error and returns default value
+
+bool b2 = int.TryParse(s1, out n3);
+Console.WriteLine($"{b2} : {n3}"); // False, 0
+
+
+
+
+
+
+
+
+
+
+string s4 = "18888";
+int i4;
+bool b4 = int.TryParse(s4, out i4);
+
+Console.WriteLine($"{b4}  : {i4}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine($"{b2} : {n3}"); // True, 1000
+
+string s3 = "True";
+// bool b3 = bool.Parse(s3);
+//Console.WriteLine(b3);
+
+bool b3 = Convert.ToBoolean(s3);
+Console.WriteLine($"{s3} : {b3}");
+
+//s3 = "456.124";
+//// double d = double.Parse(s3);
+//double d = Convert.ToDouble(s3);
+//Console.WriteLine($"{s3} : {d}");
+
+////    Convert.ToInt16() = short
+////    Convert.ToInt32() = int
+////    Convert.ToInt64() = long
+////    Convert.ToSingle() = float
+
+//string s4 = null;
+////int j1 = int.Parse(s4);
+////Console.WriteLine(j1);
+//int j2 = Convert.ToInt32(s4);
+//Console.WriteLine(j2);
+
+// diff : Parse() & Convert class method
+// Parse() method throws error when input string is null
+// Convert class method handles error and returns default value when input string is null
+
+Console.ReadLine();
