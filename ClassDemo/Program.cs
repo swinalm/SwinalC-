65d446696606d77f1a4a7ba06413e351846cf085
gauravshahare1 <121213204+gauravshahare1@users.noreply.github.com>
2024-01-25 08:28:46 +0530

    method perameter and class

---
+++ ClassDemo/Program.cs
@@ -0,0 +1,136 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int rn = 1;
+        string name = "Ramesh";
+        string gender = "Male";
+        long mobile = 8956890522;
+
+        // Student s1; // null
+
+        Student s1 = new Student(); // object / instance
+        // reading: s1 is a reference variable of type Student pointing to
+        // Student Object
+
+        s1.FirstName = "Ramesh";
+        s1.LastName = "Yadav";
+        s1.PrintFullName();
+
+        Student s3 = new Student();
+        s3.FirstName = "Tom";
+        s3.LastName = "Jerry";
+        s3.PrintFullName();
+
+
+        Student s5 = new Student()
+        {
+            FirstName = "salman",
+            LastName = "Khan"
+        };
+        s5.PrintFullName();
+
+
+
+
+        Employee employee = new Employee();
+        employee.Emp_id = 1001;
+        employee.Name = "Ranvir";
+        employee.Gender = "Male";
+
+        employee.information();
+
+        Employee e1 = new Employee()
+        {
+            Emp_id = 1002,
+            Gender = "Female",
+            Name = "Gaytri"
+        };
+        e1.information();
+
+
+        Employee swinal = new Employee()
+        {
+            Emp_id = 1003,
+            Name = "Nikhil",
+            Gender = "Male"
+        };
+
+        swinal.information();
+
+
+
+
+
+
+
+
+
+
+
+
+        Student s2 = new Student();
+        s2.FirstName = "Ritesh";
+        s2.LastName = "Jadhav";
+        s2.PrintFullName();
+
+        // object initializer syntax
+        Student s4 = new Student()
+        {
+            FirstName = "Paresh",
+            LastName = "Desale"
+        };
+        s4.PrintFullName();
+
+        Circle.Pi = 3.14f;
+
+        Circle c1 = new Circle();
+        c1.Radius = 5;
+        // c1.Pi = 3.14f;
+        c1.Area();
+
+        // Circle c2 = new Circle() { Radius = 10, Pi = 3.14f};
+        Circle c2 = new Circle() { Radius = 10 };
+        c2.Area();
+
+        Console.ReadLine();
+    }
+}
+
+public class Student
+{
+    // fields 
+    // global variable can be used anywhere inside class
+    public string FirstName;
+    public string LastName;
+
+    // method
+    public void PrintFullName()
+    {
+        Console.WriteLine($"Full Name : {FirstName} {LastName}");
+    }
+}
+
+public class Circle
+{
+    public int Radius;
+    public static float Pi;
+
+    public void Area()
+    {
+        Console.WriteLine($"Area : {Pi * Radius * Radius} of a circle having radius : {Radius} ");
+    }
+}
+
+
+public class Employee
+{
+    public string Name;
+    public int Emp_id;
+    public string Gender;
+
+    public void information()
+    {
+        Console.WriteLine($"Employee id : {Emp_id}\nName : {Name}\nGender :{Gender} ");
+    }
+}
---
+++ MethodParameter/Program.cs
@@ -0,0 +1,136 @@
+﻿class Program
+{
+    // calling method
+    static void Main()
+    {
+        #region PrintA() method call
+
+
+
+
+
+        //int a = 10; // local variable
+        //Console.WriteLine($"Before Call: {a}");// 10
+        //PrintA(a);
+        //Console.WriteLine($"After Call: {a}"); // 10
+        #endregion PrintA() method call
+
+        #region PrintB() method call
+
+        // int a; // value initailization is mandatory in calling method
+        //int a = 10;
+        //Console.WriteLine($"Before Call a : {a}"); // 10
+        //PrintB(ref a);
+        //Console.WriteLine($"After Call a : {a}"); // 1000
+
+        #endregion PrintB() method call
+
+        #region PrintC() method call
+
+        int a=10, b=200, sub, mul, div;
+        int r1 = PrintC(a, b, out sub, out mul, out div);
+        Console.WriteLine($"{a} + {b} = {r1}");
+        Console.WriteLine($"{a} - {b} = {sub}");
+        Console.WriteLine($"{a} * {b} = {mul}");
+        Console.WriteLine($"{a} / {b} = {div}");
+
+        #endregion PrintC() method call
+
+        #region PrintD() method call
+
+        //int[] numbers1 = new int[] { 10, 10, 10 };
+        //PrintD(numbers1); // 30
+
+        //PrintD(new int[] { 10, 20, 30 }); // 60
+
+        //PrintD(10, 10, 10, 10, 10);
+
+        //int[] numbers2 = null;
+        //PrintD(numbers2); // EMPTY ARRAY
+
+        //PrintD(null); // EMPTY ARRAY
+
+        //PrintE(); // NO ITEMS
+        //PrintE("A", "N", "I", "L"); // ANIL
+
+        #endregion PrintD() method call
+
+        Console.ReadLine();
+    }
+
+    static void PrintD(params int[] numbers)
+    {
+        if (numbers != null && numbers.Length > 0)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine($"Total Sum: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("EMPTY ARRAY");
+        }
+    }
+
+    static void PrintE(params string[] chars)
+    {
+        if (chars != null && chars.Length > 0)
+        {
+            string output = "";
+            for (int i = 0; i < chars.Length; i++)
+            {
+                output += chars[i];
+            }
+            Console.WriteLine(output);
+        }
+        else
+        {
+            Console.WriteLine("NO ITEMS");
+        }
+    }
+
+
+    //output parameter
+    static int PrintC(int a, int b, out int sub, out int mul, out int div)
+    {
+        sub = a - b;
+        mul = a * b;
+        div = a / b;
+        return a + b;
+    }
+
+    //static bool MyTryParse(string s, out int a)
+    //{
+
+    //}
+
+
+
+    // ref keyword
+    //static void PrintB(ref int a)
+    //{
+    //     //a = 1000; // it is not mandatory to assign value to ref parameter inside called method
+    //    Console.WriteLine($"Inside PrintB() Method a : {a}"); // 1000
+    //}
+
+
+
+
+
+
+
+
+
+
+
+    // Pass by value
+    // called method
+    //static void PrintA(int a) // method parameter
+    //{
+    //    a = 1000; // changed value of parameter
+    //    Console.WriteLine($"Value in PrintA() method : {a}");
+    //}
+}
